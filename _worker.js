ee7a10c0d70104d104095e30305602d6c1b95ee9
Russel2705 <123241549+Russel2705@users.noreply.github.com>
2024-12-19 17:23:32 +0700

    Contabo

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '143.198.213.197';
+let proxyIP = '82.197.69.135';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
