a6c8382d3c090b7f6f7c799a285929f171fe3b0a
Russel2705 <123241549+Russel2705@users.noreply.github.com>
2024-12-22 00:52:32 +0700

    Oracle SG

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '43.218.77.16';
+let proxyIP = '188.166.199.61';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
