1ed8c917db4b10a9bd773f25f653e2dbb666614d
Russel2705 <123241549+Russel2705@users.noreply.github.com>
2024-12-25 20:05:07 +0700

    Google llc id

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '160.16.123.65';
+let proxyIP = '35.219.50.99';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
